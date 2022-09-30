8580ec95113a74282c54ffa5618f13d05a083cfa
Risa <risaochiai10@gmail.com>
2022-09-29 19:53:10 -0700

    created components and added bootstraps
    Co-authored-by: Terry Park <37316422+tjpark95@users.noreply.github.com>
    Co-authored-by: Marco Xu <106225683+marcoxulin@users.noreply.github.com>
    Co-authored-by: Christopher Kim <101858037+christophminkim@users.noreply.github.com>

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Routes, Route, Link } from 'react-router-dom' ;
+import NavBar from './NavBar';
+import PostsContainer from './PostsContainer';
+import Footer from './Footer';
 
 const App = () => (
-  <div>Hello World</div>
+  <div>
+    <NavBar />
+    <PostsContainer />
+    <Footer />
+  </div>
 )
 
 export default App;
