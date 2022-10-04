a610c65255353b33335ae4164d52ea3fe9d2dabf
Terry Park <terrypark@terrys-air.lan>
2022-10-03 19:09:03 -0700

    made components by using context
    Co-authored-by: Marco Xu <106225683+marcoxulin@users.noreply.github.com>
    Co-authored-by: Risa Ochiai <06301648+risa-10@users.noreply.github.com>
    Co-authored-by: Christopher Kim <101858037+christophminkim@users.noreply.github.com>

--- client/components/PostsContainer.js
+++ client/components/PostsContainer.js
@@ -1,13 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState, useContext } from 'react';
 import {Routes, Route, Link } from 'react-router-dom' ;
 import Post from './Post';
-import Footer from './Footer'
+import Footer from './Footer';
+import { GlobalContext } from '../context/GlobalState';
+import AddButton from './AddButton';
+import NewPost from './NewPost';
 
 const PostsContainer = () => {
-const [postsList, setList] = useState([
-    {name: 'water', location: 'nook\'s cranny', type:'food/beverage', quantity: 5, description: 'bottled water'},
-    {name: 'coffee', location: 'nook\'s cranny', type:'food/beverage', quantity: 3, description: 'iced coffee'}
-  ]); 
+  // const [postsList, setList] = useState([
+  //     {name: 'water', location: 'nook\'s cranny', type:'food/beverage', quantity: 5, description: 'bottled water'},
+  //     {name: 'coffee', location: 'nook\'s cranny', type:'food/beverage', quantity: 3, description: 'iced coffee'}
+  //   ]); 
 
   // make a function that fetches the data from the database
     // use that data to update the state
@@ -16,14 +19,16 @@ const [postsList, setList] = useState([
   
   // useEffect hook?
 
-const posts = [];
-postsList.forEach((el, i) => {
-  posts.push(<Post key={i} row={i}  name={postsList[i].name} location={postsList[i].location} type={postsList[i].type} quantity={postsList[i].quantity} description={postsList[i].description} />)
-})
+  const { posts } = useContext(GlobalContext); //array of posts from the state
+  const postsList = []; // array of post components
+  posts.forEach((el, i) => {
+      postsList.push(<Post key={i} row={i}  name={posts[i].name} location={posts[i].location} type={posts[i].type} quantity={posts[i].quantity} description={posts[i].description} />)
+    })
 
 return (
   <div>
-    {posts}
+    <AddButton />
+    {postsList}
     <Footer />
   </div>
 )};
