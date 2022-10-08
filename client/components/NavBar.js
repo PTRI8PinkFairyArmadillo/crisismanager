3b8f1c9f8106a293f1b43a4452b6b91be5d2786c
Terry Park <terrypark@terrys-air.lan>
2022-10-08 10:11:39 -0700

    changed the name of the project
    Co-authored-by: Marco Xu <106225683+marcoxulin@users.noreply.github.com>
    Co-authored-by: Risa Ochiai <06301648+risa-10@users.noreply.github.com>
    Co-authored-by: Christopher Kim <101858037+christophminkim@users.noreply.github.com>

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -26,7 +26,7 @@ const NavBar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Crisis Manager</a>
+        <a class="navbar-brand" href="#">CareShare</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
